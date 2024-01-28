fed1214eb89ad066dce93daf7afebac1541e1eff
Noah <westnoahb@gmail.com>
2024-01-27 21:00:15 -0500

    repair quillpad note exporter: - fix title sanitization - handle no title case - handle default notebook case

--- src/quillpad.py
+++ src/quillpad.py
@@ -3,27 +3,42 @@ import os
 
 from colorama import Fore
 
+bad_char_list = "/\\?%*:|\"<>.;="
 
 def export(file_dictionary: dict, export_directory: str):
+    notebook_dirs: dict = {}
+
+    default_dir = f"{export_directory}/default"
+    if not os.path.exists(default_dir):
+        os.makedirs(default_dir)
+    
     for notebook in file_dictionary["notebooks"]:
+        
         print(f":: Exporting: {Fore.BLUE}{notebook['name']}{Fore.RESET}")
+        notebook_directory: str = f"{export_directory}/{notebook['name']}"
+        
+        if not os.path.exists(notebook_directory):
+            os.makedirs(notebook_directory)
+        notebook_dirs[notebook["id"]] = notebook_directory
 
     for notes in file_dictionary["notes"]:
+        try:
+            export_dir = notebook_dirs[int(notes["notebookId"])]
+        except:
+            export_dir = default_dir
+
         if "content" in notes:
-            print(f":: Exporting: {Fore.YELLOW}{notes['title']}{Fore.RESET}")
-            note_file_path: str = f"{notes['title'].upper().replace(' ', '_')}.md"
+            title = notes.get("title", str(notes.get("creationDate","no title")))
+ 
+            for bad_char in bad_char_list:
+                title.replace(bad_char, "")
+            
+            export_path: str = f"{export_dir}/{title}.md"
+            
+            print(f":: Saving {Fore.YELLOW}{title}{Fore.RESET} to {export_path}")
+            with open(export_path, "w") as note_file:
+                note_file.write(notes["content"])
 
-            for notebook in file_dictionary["notebooks"]:
-                notebook_identities = {notebook["id"]: notebook["name"]}
-                for notebook_id, notebook_name in notebook_identities.items():
-                    notebook_directory: str = f"{export_directory}/{notebook_name}"
-
-                    if not os.path.exists(notebook_directory):
-                        os.makedirs(notebook_directory)
-
-                    export_path: str = f"{notebook_directory}/{note_file_path}"
-                    print(f":: Saving to {export_path}")
-                    if int(notes["notebookId"]) == notebook_id:
-                        print(f"{notebook_id} - {notebook_name} - {notes['title']}")
-                        with open(export_path, "w") as note_file:
-                            note_file.write(notes["content"])
+if __name__ == "__main__":
+    for char in bad_char_list:
+        print(char)
