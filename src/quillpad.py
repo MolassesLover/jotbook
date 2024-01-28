d015953e421dac48dc31e6d4783fe86c34dfe565
Noah <westnoahb@gmail.com>
2024-01-27 21:24:36 -0500

    add support for quillpad lists -> markdown

--- src/quillpad.py
+++ src/quillpad.py
@@ -3,15 +3,15 @@ import os
 
 from colorama import Fore
 
-bad_char_list = "/\\?%*:|\"<>.;="
-
 def export(file_dictionary: dict, export_directory: str):
     notebook_dirs: dict = {}
 
+    # make directory for default notes
     default_dir = f"{export_directory}/default"
     if not os.path.exists(default_dir):
         os.makedirs(default_dir)
     
+    # make directory for each notebook
     for notebook in file_dictionary["notebooks"]:
         
         print(f":: Exporting: {Fore.BLUE}{notebook['name']}{Fore.RESET}")
@@ -21,24 +21,33 @@ def export(file_dictionary: dict, export_directory: str):
             os.makedirs(notebook_directory)
         notebook_dirs[notebook["id"]] = notebook_directory
 
+    #iterate over each note file and save it
     for notes in file_dictionary["notes"]:
         try:
             export_dir = notebook_dirs[int(notes["notebookId"])]
         except:
             export_dir = default_dir
 
-        if "content" in notes:
-            title = notes.get("title", str(notes.get("creationDate","no title")))
- 
-            for bad_char in bad_char_list:
-                title.replace(bad_char, "")
-            
-            export_path: str = f"{export_dir}/{title}.md"
-            
-            print(f":: Saving {Fore.YELLOW}{title}{Fore.RESET} to {export_path}")
-            with open(export_path, "w") as note_file:
-                note_file.write(notes["content"])
+        title = notes.get("title", str(notes.get("creationDate","no title")))
 
-if __name__ == "__main__":
-    for char in bad_char_list:
-        print(char)
+        # Remove characters not allowed in file names
+        bad_char_list = "/\\?%*:|\"<>.;="
+        for bad_char in bad_char_list:
+            title = title.replace(str(bad_char), "")
+        
+        export_path: str = f"{export_dir}/{title}.md"
+        
+        note_text: str = ""
+        if "content" in notes:    
+            note_text = notes["content"]
+        
+        elif "taskList" in notes:
+            for item in notes["taskList"]:
+                if item["isDone"]:
+                    note_text += f"- [x] {item.get('content', '')}\n"
+                else:
+                    note_text += f"- [ ] {item.get('content', '')}\n"
+            
+        print(f":: Saving {Fore.YELLOW}{title}{Fore.RESET} to {export_path}")
+        with open(export_path, "w") as note_file:
+            note_file.write(note_text)
